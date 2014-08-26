7eae4af4e5c118a32e9d98d970f491188ea62499
jgr <jgr006@gmail.com>
2014-08-27 07:30:56 +1000

    fixing copy script, again

--- .profile.d/mpc_controls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# vim: set ft=sh:
-
-alias mpc='mpc -f "[%position%]: %file%"'
-
-mpc_select () {
-	mpc play "$(mpc playlist | nl | grep "$@" | head -n 1 | cut -f 1)"
-}
--- copy.sh
+++ copy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-find . -name .git -a -prune \
-	-o -name bak -a -prune \
-	-o ! -name link.sh ! -name copy.sh ! -name README.md -type f \
-		-exec cp -vur ~/{} ./ \;
+find . \
+	-name "bak" -prune -o \
+	-name ".git*" -prune -o \
+		-type f \
+		! -name README.md \
+		! -name link.sh \
+		! -name copy.sh \
+		! -name ".*.swp" \
+		-exec cp -vur ~/{} {} \;
