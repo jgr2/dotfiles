ea808be8c1629ed1a593c0f54465a0ea07f12177
jgr <jgr006@gmail.com>
2014-10-14 02:01:24 +1000

    fixed issues with search/play function

--- dotfiles/.profile.d/10-mpc.sh
+++ dotfiles/.profile.d/10-mpc.sh
@@ -4,8 +4,8 @@ mpc_albums_by_artist () {
 	mpc -f "%album%" find artist $1 | uniq
 }
 
-mpc_play_file () {
-	mpc play "$(mpc -f "%file%" playlist | grep "$@" | head -n 1 | cut -d ':' -f 1)"
+mpc_gplay () {
+	mpc play "$(mpc -f "%position% %file%" playlist | grep "$@" | head -n 1 | cut -f 1)"
 }
 
 # vim: set ft=sh:
