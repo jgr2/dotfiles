5ed19aec0e5683e8d74caaa587ced434f7c94100
jgr <jgr006@gmail.com>
2014-10-24 14:55:41 +1000

    creating dwb config files

---
+++ dotfiles/.config/dwb/userscripts/defer-loading.js
@@ -0,0 +1,12 @@
+//~/.config/dwb/userscripts/defer-loading.js
+//!javascript
+if (settings.loadOnFocus === false) {
+    execute('local_set load-on-focus true');
+}
+Signal.connect('navigation', function(webview) {
+    if (webview == tabs.current) {
+        execute('local_set load-on-focus false');
+        this.disconnect();
+    }
+});
+
---
+++ dotfiles/.config/dwb/userscripts/youtube-mplayer.js
@@ -0,0 +1,11 @@
+//~/.config/dwb/userscripts/youtube-mplayer.js
+//!javascript 
+// opens YouTube videos with mplayer.
+var regex = new RegExp("http(.*)://www.youtube.com/watch\\?(.*&)*v=.*");
+
+Signal.connect("navigation", function (wv, frame, request) {
+  if (wv.mainFrame == frame && regex.test(request.uri)) 
+    system.spawn("sh -c 'mplayer \"$(youtube-dl -g " + request.uri + ")\"'");
+  return false;
+});
+
